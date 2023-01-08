53700f5c3310777d695ea213a62b122db950c64b
Adam Sturge <adamjsturge@gmail.com>
2023-01-07 20:11:53 -0500

    Add channel id to bot

--- src/bot.py
+++ src/bot.py
@@ -39,7 +39,7 @@ async def ping(ctx, *msg: str):
 @bot.command()
 async def log(ctx, *description: str):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
+    channel_id = ctx.channel.id
     description = ' '.join(description)
 
     if len(description) == 0:
@@ -50,14 +50,14 @@ async def log(ctx, *description: str):
         await ctx.send(f'{ctx.author.mention} Please keep descriptions under 2,000 characters')
         return
 
-    with get_lock(f'{user_id}:{server_id}:activities'):
-        activity.log_activity_for_user(user_id, server_id, description)
+    with get_lock(f'{user_id}:{channel_id}:activities'):
+        activity.log_activity_for_user(user_id, channel_id, description)
     await ctx.send(f'Logged activity for {ctx.author.mention}')
 
 @bot.command()
 async def logc(ctx, category_name: str, *description: str):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
+    channel_id = ctx.channel.id
     description = ' '.join(description)
 
     if len(description) == 0:
@@ -68,41 +68,44 @@ async def logc(ctx, category_name: str, *description: str):
         await ctx.send(f'{ctx.author.mention} Please keep descriptions under 2,000 characters')
         return
 
-    default = default_category.get_default_category_by_name_for_user(user_id, server_id, category_name)
-    cat = category.get_category_by_name(user_id, server_id, category_name)
+    default = default_category.get_default_category_by_name_for_user(user_id, channel_id, category_name)
+    cat = category.get_category_by_name(user_id, channel_id, category_name)
     if cat is None and default is None:
         await ctx.send(f'{ctx.author.mention} Category `{category_name}` does not exist')
         return
     else:
         cat_id = cat.id if cat is not None else None
         default_id = default.id if default is not None else None
-        with get_lock(f'{user_id}:{server_id}:activities'):
-            activity.log_activity_for_user(user_id, server_id, description, cat_id, default_id)
+        with get_lock(f'{user_id}:{channel_id}:activities'):
+            activity.log_activity_for_user(user_id, channel_id, description, cat_id, default_id)
         await ctx.send(f'Logged activity for {ctx.author.mention}')
 
 @bot.command()
-async def rmlog(ctx, index: int):
+async def rmlog(ctx, index: int, category_name: str = None):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
-    lock_key = f'{user_id}:{server_id}:activities'
+    channel_id = ctx.channel.id
+    lock_key = f'{user_id}:{channel_id}:activities'
     if is_locked(lock_key):
         await ctx.send(f'{ctx.author.mention} somehow you hit a race condition. Nothing has been removed.')
         return
 
     # Theoretically a race condition here after checking if the lock is locked before locking again
     with get_lock(lock_key):
-        activities_today = activity.get_activities_for_user_for_today(user_id, server_id)
-        if len(activities_today) <= index:
-            await ctx.send(f'{ctx.author.mention} Could not find activity with that index')
+        activities_today = activity.get_activities_for_user_for_today(user_id, channel_id)
+        if category_name not in activities_today:
+            await ctx.send(f'{ctx.author.mention} You do not have an activity in that category')
             return
-        activity.remove_activity(activities_today[index].id)
+        if len(activities_today[category_name]) <= index:
+            await ctx.send(f'{ctx.author.mention} Could not find an activity in the category')
+            return
+        activity.remove_activity(activities_today[category_name][index].id)
     await ctx.send(f'{ctx.author.mention} Removed activity at index {index}')
 
 @bot.command()
 async def show(ctx):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
-    activities_today = activity.get_activities_for_user_for_today(user_id, server_id)
+    channel_id = ctx.channel.id
+    activities_today = activity.get_activities_for_user_for_today(user_id, channel_id)
 
     description = ''
     if len(activities_today) == 0:
@@ -121,29 +124,6 @@ async def show(ctx):
         embed.add_field(name=escaped_category, value=description, inline=False)
     await ctx.send(embed=embed)
 
-# @bot.command()
-# async def edit(ctx, index: int, *new_description: str):
-#     user_id = ctx.author.id
-#     server_id = ctx.guild.id
-#     new_description = ' '.join(new_description)
-
-#     if len(new_description) > 2000:
-#         await ctx.send(f'{ctx.author.mention} Please keep descriptions under 2,000 characters')
-#         return
-
-#     if len(new_description) == 0:
-#         await ctx.send(f'{ctx.author.mention} Description cannot be empty')
-#         return
-
-#     activities_today = activity.get_activities_for_user_for_today(user_id, server_id)
-#     if len(activities_today) <= index:
-#         await ctx.send(f'{ctx.author.mention} Could not find activity with that index')
-#         return
-
-#     with get_lock(f'{user_id}:{server_id}:activities'):
-#         activity.update_activity_description(activities_today[index].id, new_description)
-#     await ctx.send(f'{ctx.author.mention} Activity updated')
-
 @bot.command()
 async def settz(ctx, timezone: str):
     user_id = ctx.author.id
@@ -156,7 +136,7 @@ async def settz(ctx, timezone: str):
 @bot.command()
 async def addcat(ctx, name: str = ''):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
+    channel_id = ctx.channel.id
     name = name.strip()
 
     if len(name) == 0:
@@ -169,14 +149,14 @@ async def addcat(ctx, name: str = ''):
 
     default_cat = default_category.get_default_category_by_name(name)
     if default_cat is not None:
-        if default_category.is_user_opted_out_of_default_category(user_id, server_id, default_cat.id):
-            default_category.opt_into_default_category(user_id, server_id, default_cat.id)
+        if default_category.is_user_opted_out_of_default_category(user_id, channel_id, default_cat.id):
+            default_category.opt_into_default_category(user_id, channel_id, default_cat.id)
             await ctx.send(f'{ctx.author.mention} Category created')
         else:
             await ctx.send(f'{ctx.author.mention} `{name}` already exists as a default category')
         return
 
-    result = category.create_category_for_user(user_id, server_id, name)
+    result = category.create_category_for_user(user_id, channel_id, name)
     if result is None:
         await ctx.send(f'{ctx.author.mention} Category already exists')
     else:
@@ -185,7 +165,7 @@ async def addcat(ctx, name: str = ''):
 @bot.command()
 async def editcat(ctx, old_name: str, new_name: str):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
+    channel_id = ctx.channel.id
     new_name = new_name.strip()
 
     if len(new_name) == 0:
@@ -196,7 +176,7 @@ async def editcat(ctx, old_name: str, new_name: str):
         await ctx.send(f'{ctx.author.mention} Category name must be 50 characters or fewer')
         return
 
-    result = category.get_category_by_name(user_id, server_id, new_name)
+    result = category.get_category_by_name(user_id, channel_id, new_name)
     if result is not None:
         await ctx.send(f'{ctx.author.mention} Category `{new_name}` already exists')
         return
@@ -206,7 +186,7 @@ async def editcat(ctx, old_name: str, new_name: str):
         await ctx.send(f'{ctx.author.mention} `{new_name}` already exists as a default category')
         return
 
-    result = category.get_category_by_name(user_id, server_id, old_name)
+    result = category.get_category_by_name(user_id, channel_id, old_name)
     if result is None:
         await ctx.send(f'{ctx.author.mention} Category does not exist')
         return
@@ -217,22 +197,22 @@ async def editcat(ctx, old_name: str, new_name: str):
 @bot.command()
 async def rmcat(ctx, name: str, force: str = None):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
+    channel_id = ctx.channel.id
 
     result = default_category.get_default_category_by_name(name)
     if result is not None:
-        if default_category.is_category_being_used_by_activity(user_id, server_id, result.id) and force != 'FORCE':
+        if default_category.is_category_being_used_by_activity(user_id, channel_id, result.id) and force != 'FORCE':
             await ctx.send(f'{ctx.author.mention} That category has activities associated with it. Run `;rmcat "{name}" FORCE` to force removal. Activities with this category will have the category removed.')
         else:
-            default_category.opt_out_of_default_category(user_id, server_id, result.id)
+            default_category.opt_out_of_default_category(user_id, channel_id, result.id)
             await ctx.send(f'{ctx.author.mention} Category `{name}` deleted')
     else:
-        result = category.get_category_by_name(user_id, server_id, name)
+        result = category.get_category_by_name(user_id, channel_id, name)
         if result is None:
             if result is None:
                 await ctx.send(f'{ctx.author.mention} Category does not exist')
         else:
-            if category.is_category_being_used_by_activity(user_id, server_id, result.id) and force != 'FORCE':
+            if category.is_category_being_used_by_activity(user_id, channel_id, result.id) and force != 'FORCE':
                 await ctx.send(f'{ctx.author.mention} That category has activities associated with it. Run `;rmcat "{name}" FORCE` to force removal. Activities with this category will have the category removed.')
             else:
                 category.delete_category(result.id)
@@ -241,9 +221,9 @@ async def rmcat(ctx, name: str, force: str = None):
 @bot.command()
 async def lscats(ctx):
     user_id = ctx.author.id
-    server_id = ctx.guild.id
-    custom_categories = category.get_categories_for_user(user_id, server_id)
-    default_categories = default_category.get_default_categories_for_user(user_id, server_id)
+    channel_id = ctx.channel.id
+    custom_categories = category.get_categories_for_user(user_id, channel_id)
+    default_categories = default_category.get_default_categories_for_user(user_id, channel_id)
 
     if len(custom_categories) == 0 and len(default_categories) == 0:
         await ctx.send(f'{ctx.author.mention} No categories defined yet')

8fbf38a8dc7306da5446f601411243d8db072306
SnowySailor <kanel.nick@gmail.com>
2023-05-04 01:51:01 -0400

    update to discordpy 2

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -12,7 +12,6 @@ def init_logger():
     global logger
     logger = logging.getLogger('discord')
     logger.setLevel(logging.INFO)
-    sys.stderr = LoggerWriter(logger.error)
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)

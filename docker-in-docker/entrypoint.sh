593b3edd76d77eb9972da67690a635d409d57fab
SnowySailor <kanel.nick@gmail.com>
2024-05-24 15:49:29 -0400

    rewrite entrypoint

--- docker-in-docker/entrypoint.sh
+++ docker-in-docker/entrypoint.sh
@@ -1,6 +1,27 @@
-dockerd-entrypoint.sh &
-while ! /usr/local/bin/docker info >/dev/null; do
+#!/bin/bash
+
+retry_limit=12
+retry_count=0
+
+start_docker() {
+    dockerd-entrypoint.sh &
+}
+
+check_docker() {
+    /usr/local/bin/docker info >/dev/null
+}
+
+start_docker
+
+while ! check_docker; do
     echo "docker not running yet. Waiting..." >> /proc/1/fd/1;
     sleep 5;
-done;
-cd /app && docker-compose up;
+    retry_count=$((retry_count+1))
+    if [ $retry_count -ge $retry_limit ]; then
+        echo "Exceeded retry limit. Restarting docker..." >> /proc/1/fd/1;
+        retry_count=0
+        start_docker
+    fi
+done
+
+cd /app && docker-compose up
